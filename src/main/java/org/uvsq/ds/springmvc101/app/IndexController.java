de0be5cc533945afecaaf488085b2eb35213a46c
Marc DeXeT <marc.dexet@ias.u-psud.fr>
2019-12-28 16:01:44 +0100

    First JSP with model

--- src/main/java/org/uvsq/ds/springmvc101/app/IndexController.java
+++ src/main/java/org/uvsq/ds/springmvc101/app/IndexController.java
@@ -1,23 +1,40 @@
 package org.uvsq.ds.springmvc101.app;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 	private Logger logger = LoggerFactory.getLogger(IndexController.class);
-	
+
+	private final String message = "Celui qui déplace la montagne, c’est celui qui commence à enlever les petites pierres";
+
 	public IndexController() {
 		logger.info("IndexController Creation");
 	}
 
-	
-	@RequestMapping({"/", "/index"})
+	@RequestMapping({ "/", "/index" })
 	public ModelAndView index() {
-		return new ModelAndView("index");
+		return new ModelAndView("index", "welcome_message", message);
+	}
+
+	@RequestMapping({ "/index-mav" })
+	public ModelAndView indexModelAndView() {
+		Map<String, String> model = new HashMap<>();
+		model.put("welcome_message", message);
+		return new ModelAndView("index", model);
 	}
 	
+	@RequestMapping({"/index-str"})
+	public String indexString(Model model) {
+	  model.addAttribute("welcome_message", message);
+	  return "index";
+	}
 }
--- src/main/java/org/uvsq/ds/springmvc101/config/WebConfig.java
+++ src/main/java/org/uvsq/ds/springmvc101/config/WebConfig.java
@@ -2,9 +2,13 @@ package org.uvsq.ds.springmvc101.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 import org.uvsq.ds.springmvc101.ScanPackageRoot;
 
 @EnableWebMvc
@@ -16,6 +20,15 @@ public class WebConfig {
 	
 	public WebConfig() {
 		logger.info("WebConfig Creation");
+	}
+	
+	@Bean
+	public ViewResolver jspViewResolver() {
+	    InternalResourceViewResolver bean = new InternalResourceViewResolver();
+	    bean.setViewClass(JstlView.class);
+	    bean.setPrefix("/WEB-INF/views/");
+	    bean.setSuffix(".jsp");
+	    return bean;
 	}
 
 }
