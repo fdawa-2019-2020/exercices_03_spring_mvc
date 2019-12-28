24284d442415f0386deb044a132fbac9633ae1ab
Marc DeXeT <marc.dexet@ias.u-psud.fr>
2019-12-28 18:31:08 +0100

    End of POST

--- src/main/java/org/uvsq/ds/springmvc101/project/ProjectController.java
+++ src/main/java/org/uvsq/ds/springmvc101/project/ProjectController.java
@@ -1,23 +1,42 @@
 package org.uvsq.ds.springmvc101.project;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProjectController {
-	
+
 	private ProjectService projectService;
 
 	public ProjectController(ProjectService projectService) {
 		this.projectService = projectService;
 	}
 
-	@RequestMapping("/projects")
+	@GetMapping("/projects")
 	@ResponseBody
 	public List<Project> getAllProjects() {
 		return projectService.getAllProjects();
+	}
+
+	@PostMapping("/projects-random")
+	@ResponseBody
+	public Project createRandomProject() {
+		Project project = new Project();
+		project.setName(UUID.randomUUID().toString());
+		project.setNature(Nature.OPEN);
+		Project created = projectService.createProject(project);
+		return created;
+	}
+
+	@PostMapping("/projects")
+	@ResponseBody
+	public Project createProject(@RequestBody Project project) {
+		return projectService.createProject(project);
 	}
 }
