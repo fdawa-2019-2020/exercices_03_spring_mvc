728d12dc047d3fdbdb0430dfd467342910c6eacf
Marc DeXeT <marc.dexet@ias.u-psud.fr>
2019-12-29 21:47:11 +0100

    Serial XML. POST. path variable 1

--- src/main/java/org/uvsq/ds/springmvc101/project/ProjectController.java
+++ src/main/java/org/uvsq/ds/springmvc101/project/ProjectController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,5 +39,10 @@ public class ProjectController {
 	@ResponseBody
 	public Project createProject(@RequestBody Project project) {
 		return projectService.createProject(project);
+	}
+	
+	@GetMapping("/projects/{id}")
+	@ResponseBody Project getProjectById(@PathVariable Long id) {
+		return projectService.getProjectById(id);
 	}
 }
--- src/main/java/org/uvsq/ds/springmvc101/project/ProjectService.java
+++ src/main/java/org/uvsq/ds/springmvc101/project/ProjectService.java
@@ -5,6 +5,7 @@ import static java.util.stream.Collectors.toList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 
@@ -26,6 +27,11 @@ public class ProjectService {
 			projectByName.put(name, p);
 		}
 		return p;
+	}
+	
+	public Project getProjectById(Long id) {
+		Optional<Project> op = projectByName.values().stream().filter(p -> p.getId().equals(id)).findFirst();
+		return op.orElse(null);
 	}
 
 }
